d4be00affc8ee7ebf7dd01126980f8b432131d94
ulla.volke <ulla.volke@khk.ee>
2019-01-10 11:46:20 +0200

    Add posts listing functionality

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -3,7 +3,7 @@
     <div class="span8">
         <h1><? echo $post['post_subject']?></h1>
         <p><? echo $post['post_text']?></p>
-        <div> 
+        <div>
             <span class="badge badge-success">Posted <?php echo $post['post_created']; ?></span><div class="pull-right"><span class="label label-primary">alice</span> <span class="label label-primary">story</span> <span class="label label-primary">blog</span> <span class="label label-primary">personal</span></div>
         </div>
         <hr>
